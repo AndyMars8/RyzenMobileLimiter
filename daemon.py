ccff8c249c466f3e35e1464961c1228a63a02501
AndyMars8 <andes.ma@outlook.com>
2026-03-14 21:27:15 +1000

    Added logging to daemon

--- daemon.py
+++ daemon.py
@@ -1,7 +1,69 @@
 # This program is the daemon for ryzenm-limit
 
-import fcntl, os, sys, signal, time, shutil, subprocess, ctypes
+import fcntl, os, sys, signal, time, shutil, subprocess, ctypes, logging, atexit
+import logging.config
 from parse_args import RuntimeCheck
+
+
+logger = logging.getLogger("ryzenm-limit")
+logging_config = {
+    "version": 1,
+    "disable_existing_loggers": True,
+    "formatters": {
+        "simple": {
+            "format": "[%(levelname)s] %(asctime)s - %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S"
+        }
+    },
+    "handlers": {
+        "stdout": {
+            "class": "logging.StreamHandler",
+            "level": "INFO",
+            "formatter": "simple",
+            "stream": "ext://sys.stdout"
+        },
+        "stderr": {
+            "class": "logging.StreamHandler",
+            "level": "WARNING",
+            "formatter": "simple",
+            "stream": "ext://sys.stderr"
+        },
+        "file": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "level": "DEBUG",
+            "formatter": "simple",
+            "filename": "logs/ryzenm-limit.log",
+            "maxBytes": 200000,
+            "backupCount": 2
+        },
+        "queue_handler": {
+            "class": "logging.handlers.QueueHandler",
+            "handlers": [
+                "stdout",
+                "stderr",
+                "file"
+            ],
+            "respect_handler_level": True
+        }
+    },
+    "loggers": {
+        "ryzenm-limit": {
+            "level": "DEBUG",
+            "handlers": [
+                "queue_handler"
+            ]
+        }
+    }
+}
+
+
+def logging_setup():
+    os.makedirs(os.path.dirname(os.path.abspath(__file__)) + "/logs", exist_ok=True)
+    logging.config.dictConfig(logging_config)
+    queue_handler = logging.getHandlerByName("queue_handler")
+    if queue_handler is not None:
+        queue_handler.listener.start()
+        atexit.register(queue_handler.listener.stop)
 
 
 class DaemonHelper:
@@ -16,7 +78,8 @@ class DaemonHelper:
 
         self.init_ryzenadj()
         self.retrieve_settings()
-        self.apply_settings()
+        # print("Applying settings at launch")
+        # self.apply_settings()
 
     def run_once(self, lockfile):
         fd = os.open(lockfile, os.O_CREAT | os.O_RDWR)
@@ -62,12 +125,14 @@ class DaemonHelper:
     def monitor(self):
         self.lib.refresh_table(self.ryzenadj)
         if self.lib.get_tctl_temp(self.ryzenadj) != self.settings["tctl_temp"]:    # Reapply user limits if a reset has been detected or user has changed settings
+            print("Applying settings for persistence")
             self.apply_settings()
         time.sleep(1)
         # Check if config content has changed
         current_mtime = os.path.getmtime(RuntimeCheck.get_config_path())
         if current_mtime != self.last_mtime:
             self.retrieve_settings()
+            print("Applying settings due to config change")
             self.apply_settings()
             self.last_mtime = current_mtime
 
@@ -106,7 +171,7 @@ class DaemonHelper:
                 if p == 'temp-limit':
                     self.settings["tctl_temp"] = int(params[p])
                 else:
-                    self.settings[p.replace('-', '_')] = int(params[p]) * 1000
+                    self.settings[p.replace('-', '_')] = int(params[p]) * 1000  # Convert W to mW
             except:
                 print(f"Invalid value: {params[p]} detected for {p}")
 
@@ -124,10 +189,13 @@ if __name__ == "__main__":
         print("Root privileges are required")
         sys.exit(1)
 
+    print(os.getpid())
+
     d = DaemonHelper()
 
     try:
-        print(f"Running RyzenMobileLimiter daemon")
+        logging_setup()
+        logger.info(f"Started RyzenMobileLimiter daemon")
         while True:
             time.sleep(1)
             d.monitor()
